147ed98c44268601effbf215c53e30fc9e1671a8
nachiketam7734 <nachiketam7734@gmail.com>
2022-07-27 20:57:24 +0530

    stocks and mutal funds with change name

--- DailyMutualFundNAV/src/main/resources/data.sql
+++ DailyMutualFundNAV/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 drop table if exists mutualfund;
 create table mutualfund(mutual_Fund_Id int primary key , mutual_Fund_Name varchar(30),mutual_Fund_Value double);
 
-insert into mutualfund values(11,'GLE',FLOOR(RAND()*(1000-500+1)+500));
-insert into mutualfund values(12,'FBE',FLOOR(RAND()*(2000-1000+1)+1000));
-insert into mutualfund values(13,'SLJ',FLOOR(RAND()*(1000-500+1)+500));
-insert into mutualfund values(14,'SRD',FLOOR(RAND()*(2000-1000+1)+1000));
-insert into mutualfund values(15,'QSF',FLOOR(RAND()*(1000-500+1)+500));
-insert into mutualfund values(16,'CPE',FLOOR(RAND()*(3000-2000+1)+2000));
+insert into mutualfund values(11,'HDFCAMC',FLOOR(RAND()*(1000-500+1)+500));
+insert into mutualfund values(12,'MIRAGEASSET',FLOOR(RAND()*(2000-1000+1)+1000));
+insert into mutualfund values(13,'TPCC',FLOOR(RAND()*(1000-500+1)+500));
+insert into mutualfund values(14,'AXXA',FLOOR(RAND()*(2000-1000+1)+1000));
+insert into mutualfund values(15,'QUANT',FLOOR(RAND()*(1000-500+1)+500));
+insert into mutualfund values(16,'ICCI',FLOOR(RAND()*(3000-2000+1)+2000));
 insert into mutualfund values(17,'GLA',FLOOR(RAND()*(2000-1000+1)+1000));
-insert into mutualfund values(18,'ART',FLOOR(RAND()*(3000-2000+1)+2000));
-insert into mutualfund values(19,'URE',FLOOR(RAND()*(1000-500+1)+500));
+insert into mutualfund values(18,'AMC',FLOOR(RAND()*(3000-2000+1)+2000));
+insert into mutualfund values(19,'LNCT',FLOOR(RAND()*(1000-500+1)+500));
 insert into mutualfund values(20,'TATA',FLOOR(RAND()*(1000-500+1)+500));
 
--- DailySharePrice/src/main/resources/data.sql
+++ DailySharePrice/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 drop table if exists stocklist;
 create table stocklist(stock_id int primary key, stock_name varchar(20),stock_value double);
 
-insert into stocklist values(51,'DLF',FLOOR(RAND()*(2000-1000+1)+1000));
-insert into stocklist values(52,'TECHM',FLOOR(RAND()*(1000-500+1)+500));
-insert into stocklist values(53,'BEL',FLOOR(RAND()*(1000-500+1)+500));
-insert into stocklist values(54,'SBI',FLOOR(RAND()*(2000-500+1)+500));
-insert into stocklist values(55,'CTS',FLOOR(RAND()*(1000-500+1)+500));
-insert into stocklist values(56,'SRT',FLOOR(RAND()*(1000-500+1)+500));
-insert into stocklist values(57,'DHF',FLOOR(RAND()*(2000-500+1)+500));
-insert into stocklist values(58,'CSR',FLOOR(RAND()*(1000-500+1)+500));
-insert into stocklist values(59,'CDE',FLOOR(RAND()*(3000-2000+1)+2000));
-insert into stocklist values(60,'ADM',FLOOR(RAND()*(4000-3000+1)+3000));
-insert into stocklist values(61,'JSR',FLOOR(RAND()*(1000-500+1)+500));
+insert into stocklist values(51,'TATAMOTORS',FLOOR(RAND()*(2000-1000+1)+1000));
+insert into stocklist values(52,'TCS',FLOOR(RAND()*(1000-500+1)+500));
+insert into stocklist values(53,'COGNIZANT',FLOOR(RAND()*(1000-500+1)+500));
+insert into stocklist values(54,'MICROSOFT',FLOOR(RAND()*(2000-500+1)+500));
+insert into stocklist values(55,'APPLE',FLOOR(RAND()*(1000-500+1)+500));
+insert into stocklist values(56,'FACEBOOK',FLOOR(RAND()*(1000-500+1)+500));
+insert into stocklist values(57,'ALIBABA',FLOOR(RAND()*(2000-500+1)+500));
+insert into stocklist values(58,'BEL',FLOOR(RAND()*(1000-500+1)+500));
+insert into stocklist values(59,'ZOMATO',FLOOR(RAND()*(3000-2000+1)+2000));
+insert into stocklist values(60,'PAYTM',FLOOR(RAND()*(4000-3000+1)+3000));
+insert into stocklist values(61,'TANLA',FLOOR(RAND()*(1000-500+1)+500));
