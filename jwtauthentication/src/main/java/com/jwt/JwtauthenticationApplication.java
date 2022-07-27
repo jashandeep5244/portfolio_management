be526b858ba0efcbd2b8bb16c0cfe5da0e28e37b
Jashandeep <csecec.1802060@gmail.com>
2022-07-27 12:44:10 +0530

    Revert "resorting conflict"
    This reverts commit 3677ee7713d59aa603526db4cad7935ad40e16c8.

--- jwtauthentication/src/main/java/com/jwt/JwtauthenticationApplication.java
+++ jwtauthentication/src/main/java/com/jwt/JwtauthenticationApplication.java
@@ -5,12 +5,10 @@ import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.context.SecurityContext;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
